501720597fac460eb5993771ec0c7e60563011b7
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2024-02-04 17:17:53 +0100

    created create mapping for poker game

---
+++ src/main/java/de/stammtisch/pokerstats/controllers/apis/PokerGameController.java
@@ -0,0 +1,31 @@
+package de.stammtisch.pokerstats.controllers.apis;
+
+import de.stammtisch.pokerstats.controllers.dtos.CreatePokerGameRequest;
+import de.stammtisch.pokerstats.service.PokerGameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/api/v1/pokerGame")
+public class PokerGameController {
+    private final PokerGameService pokerGameService;
+
+    @Autowired
+    public PokerGameController(PokerGameService pokerGameService) {
+        this.pokerGameService = pokerGameService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<String> create(@RequestBody CreatePokerGameRequest request) {
+        try {
+            this.pokerGameService.createGame(request);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Successfully created Poker Game.", HttpStatus.CREATED);
+    }
+}
