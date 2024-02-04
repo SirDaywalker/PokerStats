79ebb1cbdd9891d3e32c7f4cdf8e2f163595fae2
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-04 13:08:46 +0100

    Initilized spring boot application

---
+++ src/main/java/de/stammtisch/pokerstats/PokerStatsApplication.java
@@ -0,0 +1,13 @@
+package de.stammtisch.pokerstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerStatsApplication.class, args);
+	}
+
+}
