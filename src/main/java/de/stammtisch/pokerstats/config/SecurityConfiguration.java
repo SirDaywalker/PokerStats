b6f65f73e281a482efcac4a55113de51f5280b67
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-04 15:22:22 +0100

    Added securityFilterChain

---
+++ src/main/java/de/stammtisch/pokerstats/config/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package de.stammtisch.pokerstats.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    private final JWTAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public SecurityConfiguration(
+            JWTAuthenticationFilter jwtAuthenticationFilter,
+            AuthenticationProvider authenticationProvider
+    ) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(@NonNull HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(auth -> {
+           auth.requestMatchers(
+                   "/",
+                   "/login",
+                   "/register",
+                   "/error",
+                   "/api/v1/auth/register",
+                   "/api/v1/auth/authenticate",
+                   "/cdn/**",
+                   "/js/**",
+                   "/css/**",
+                   "/img/**"
+           ).permitAll();
+           auth.anyRequest().authenticated();
+        })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(this.authenticationProvider)
+                .addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin(form -> form.loginPage("/login"))
+                .build();
+    }
+}
