8b7b2446f772eff0673290ed8a6f8776a5f036d7
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-04 15:42:08 +0100

    Added sql getter to user repo

--- src/main/java/de/stammtisch/pokerstats/repository/UserRepository.java
+++ src/main/java/de/stammtisch/pokerstats/repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByName(String name);
+    boolean existsByName(String name);
 }
