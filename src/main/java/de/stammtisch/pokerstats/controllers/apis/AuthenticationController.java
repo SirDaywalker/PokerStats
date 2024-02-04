72aa507872faaca6715cee2f36d681c2bed54b18
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2024-02-04 15:36:38 +0100

    added authentication mapping

--- src/main/java/de/stammtisch/pokerstats/controllers/apis/AuthenticationController.java
+++ src/main/java/de/stammtisch/pokerstats/controllers/apis/AuthenticationController.java
@@ -1,5 +1,37 @@
 package de.stammtisch.pokerstats.controllers.apis;
 
-public class AuthenticationController {
+import de.stammtisch.pokerstats.controllers.dtos.AuthenticationRequest;
+import de.stammtisch.pokerstats.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("authenticate")
+    public ResponseEntity<String> authenticate (
+            @RequestBody AuthenticationRequest request,
+            HttpServletResponse response
+    ) {
+        final String token;
+        try {
+            token = this.authenticationService.authenticate(request);
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        response.addCookie(AuthenticationService.generateCookie(""));
+        return new ResponseEntity<>("Successfully authenticated.", HttpStatus.OK);
+    }
 }
