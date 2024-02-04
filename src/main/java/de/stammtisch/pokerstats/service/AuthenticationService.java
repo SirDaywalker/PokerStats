66719d8db1eb47768b8c0a1bc454324102b3f005
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2024-02-04 15:50:16 +0100

    added authentication logic

--- src/main/java/de/stammtisch/pokerstats/service/AuthenticationService.java
+++ src/main/java/de/stammtisch/pokerstats/service/AuthenticationService.java
@@ -1,8 +1,14 @@
 package de.stammtisch.pokerstats.service;
 
+import de.stammtisch.pokerstats.controllers.dtos.AuthenticationRequest;
+import de.stammtisch.pokerstats.models.User;
 import de.stammtisch.pokerstats.repository.UserRepository;
+import jakarta.servlet.http.Cookie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +30,58 @@ public class AuthenticationService {
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
+    }
+
+    /**
+     * Generates the Cookie to safe the authentication to access the site.
+     *
+     * @param token the token
+     * @return the freshly baked cookie.
+     */
+    public static @NonNull Cookie generateCookie(String token) {
+        Cookie cookie = new Cookie("Authorization", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 60 * 24 * 7);
+        cookie.setAttribute("SameSite", "Strict");
+        return cookie;
+    }
+
+    /**
+     * Authenticates the user.
+     *
+     * @param authenticationRequest The authentication request.
+     * @return The generated token.
+     */
+    public String authenticate(@NonNull AuthenticationRequest authenticationRequest) {
+        this.matchCredentials(authenticationRequest.name(), authenticationRequest.password());
+        final User user = this.userRepository.findByName(authenticationRequest.name()).orElseThrow();
+        return this.generateToken(user);
+    }
+
+    /**
+     * Matches the credentials against the database.
+     *
+     * @param name The name to match.
+     * @param password The password to match.
+     * @throws BadCredentialsException If the credentials are not valid.
+     */
+    public void matchCredentials(String name, String password) throws BadCredentialsException {
+        this.authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        name,
+                        password
+                )
+        );
+    }
+
+    /**
+     * Generates a token for the given user.
+     *
+     * @param user The user to generate the token for.
+     * @return The generated token.
+     */
+    public String generateToken(@NonNull User user) {
+        return this.jwtService.generateToken(user);
     }
 }
