b3092e60914768ee305023c3ad5954a8ea643658
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-06 14:12:39 +0100

    Replaced camel-case from mappijng

--- src/main/java/de/stammtisch/pokerstats/controller/api/PokerGameController.java
+++ src/main/java/de/stammtisch/pokerstats/controller/api/PokerGameController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/api/v1/pokerGame")
+@RequestMapping("/api/v1/poker-game")
 public class PokerGameController {
     private final PokerGameService pokerGameService;
 
