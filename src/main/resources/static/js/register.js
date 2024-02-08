4c86fffbd7ee5a450461751d8db2966fda60867e
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-08 19:24:38 +0100

    Added register functionality

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,8 +1,34 @@
-document.getElementById('image-upload').addEventListener('change', function() {
+document.getElementById('picture').addEventListener('change', function() {
     let reader = new FileReader();
     reader.onload = function(e) {
         document.getElementById('image-preview').style.display = 'block';
         document.getElementById('image-preview').src = e.target.result;
     };
     reader.readAsDataURL(this.files[0]);
+});
+
+const form = document.getElementsByTagName('form')[0];
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const password = document.getElementById('password').value;
+    const picture = document.getElementById('picture').files[0];
+
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('password', password);
+    formData.append('picture', picture);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/api/v1/auth/register', true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            window.location.href = '/';
+        } else {
+            alert('Error: ' + xhr.responseText);
+        }
+    };
+    xhr.send(formData);
 });
