a32d1b22d0344a23aeee7c2fe325153174ba8836
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-13 21:10:04 +0100

    Added animation to error message

--- src/main/resources/static/js/setErrorMessage.js
+++ src/main/resources/static/js/setErrorMessage.js
@@ -1,8 +1,17 @@
 export function setErrorMessage(message) {
     const errorElement = document.getElementsByClassName("message")[0];
+    let styleModified = false;
+
     if (errorElement.classList.contains('red')) {
+        errorElement.style.display = 'block';
+        styleModified = true;
         errorElement.classList.remove('red');
     }
-    errorElement.classList.add('red');
+    setTimeout(function() {
+        errorElement.classList.add('red');
+        if (styleModified) {
+            errorElement.style.display = '';
+        }
+    }, 10);
     errorElement.innerHTML = message;
 }
