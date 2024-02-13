bc0a12a48c613f0ea380c59a79bfe57fda1540bc
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-13 19:13:46 +0100

    Continued work on register page

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,4 +1,4 @@
-document.getElementById('picture').addEventListener('change', function() {
+document.getElementById('profile-image-selector').addEventListener('change', function() {
     let reader = new FileReader();
     reader.onload = function(e) {
         document.getElementById('image-preview').style.display = 'block';
@@ -14,7 +14,15 @@ form.addEventListener('submit', function(event) {
 
     const name = document.getElementById('name').value;
     const password = document.getElementById('password').value;
-    const picture = document.getElementById('picture').files[0];
+    const picture = document.getElementById('profile-image-selector').files[0];
+    const confirmPassword = document.getElementById('password-confirm').value;
+
+    if (password !== confirmPassword) {
+        const errorElement = document.getElementsByClassName('message')[0];
+        errorElement.innerHTML = 'Passwörter stimmen nicht überein.';
+        errorElement.classList.add('red');
+        return;
+    }
 
     const formData = new FormData();
     formData.append('name', name);
@@ -26,8 +34,22 @@ form.addEventListener('submit', function(event) {
     xhr.onload = function() {
         if (xhr.status === 200) {
             window.location.href = '/';
+            return;
+        }
+        const errorElement = document.getElementsByClassName('message')[0];
+        if (errorElement.classList.contains('red')) {
+            errorElement.classList.remove('red');
+        }
+        errorElement.classList.add('red');
+        if (xhr.status === 400) {
+            errorElement.innerHTML = 'Ein Fehler ist aufgetreten. Existiert ein Account für Sie bereits?';
+        }
+        else if (xhr.status === 413) {
+            errorElement.innerHTML = 'Das Bild ist zu groß. Bitte wählen Sie ein Bild mit einer kleineren Dateigröße.';
         } else {
-            alert('Error: ' + xhr.responseText);
+            const errorElement = document.getElementsByClassName('message')[0];
+            errorElement.innerHTML = xhr.status.toString();
+            errorElement.classList.add('red');
         }
     };
     xhr.send(formData);
