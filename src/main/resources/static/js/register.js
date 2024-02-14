24e813320f698954dbcc1b087cd30cd54ef6a134
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-14 18:07:16 +0100

    Added password weakness bar

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -10,16 +10,29 @@ document.getElementById('profile-image-selector').addEventListener('change', fun
 });
 
 const form = document.getElementsByTagName('form')[0];
+const password = document.getElementById('password');
+
+password.addEventListener('input', function() {
+    let input = this.value;
+    let percentage = 0;
+
+    percentage += Number(new RegExp(/[A-Z]/).test(input)) * 25;
+    percentage += Number(new RegExp(/[a-z]/).test(input)) * 25;
+    percentage += Number(new RegExp(/[0-9]/).test(input)) * 25;
+    percentage += Number(new RegExp(/[^A-Za-z0-9]/).test(input)) * 25;
+    percentage *= Math.min(input.length / 8, 1)
+
+    document.getElementById('bar').style.width = percentage + '%';
+});
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const name = document.getElementById('name').value;
-    const password = document.getElementById('password').value;
     const picture = document.getElementById('profile-image-selector').files[0];
     const confirmPassword = document.getElementById('password-confirm').value;
 
-    if (password !== confirmPassword) {
+    if (password.value !== confirmPassword) {
         const errorElement = document.getElementsByClassName('message')[0];
         errorElement.innerHTML = 'Passwörter stimmen nicht überein.';
         errorElement.classList.add('red');
@@ -28,7 +41,7 @@ form.addEventListener('submit', function(event) {
 
     const formData = new FormData();
     formData.append('name', name);
-    formData.append('password', password);
+    formData.append('password', password.value);
     formData.append('picture', picture);
 
     const xhr = new XMLHttpRequest();
