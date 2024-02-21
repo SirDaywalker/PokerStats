0aca5ee745e786f0ff003a3fd823eb1f38ed705f
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-21 16:17:06 +0100

    Added new request keys to edit-acc dto

--- src/main/java/de/stammtisch/pokerstats/controller/dtos/EditAccountRequest.java
+++ src/main/java/de/stammtisch/pokerstats/controller/dtos/EditAccountRequest.java
@@ -2,11 +2,13 @@ package de.stammtisch.pokerstats.controller.dtos;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public record EditAccountRequest(
+public record EditAccountRequest (
         String name,
         String password,
         String newPassword,
         int buyIn,
         MultipartFile picture,
-        String email
+        String email,
+        String role,
+        String targetName
 ) {}
