da95f64025f0a652b738d7e4691790c592e4744b
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-02-28 09:28:55 +0100

    Added pot calc to poker game creation

--- src/main/resources/static/js/new-poker-game.js
+++ src/main/resources/static/js/new-poker-game.js
@@ -1,6 +1,33 @@
 const users_element = document.getElementById('users');
 const selected_users = document.getElementById('selected');
 
+const potElement = document.getElementById('total-pot');
+const payout = document.getElementById('winning-amount');
+
+let defaultPot = "";
+for (let char of potElement.value) {
+    if (char === '€') {
+        break;
+    }
+    defaultPot += char;
+}
+defaultPot = Number(defaultPot);
+payout.value = (defaultPot / 2).toFixed(2) + "€";
+
+function updatePot() {
+    let pot = defaultPot;
+    for (let user of selected_users.children) {
+        if (user.localName === "span") {
+            continue;
+        }
+        pot += Number(user.children[3].innerText);
+    }
+    potElement.value = String(pot.toFixed(2)) + "€ im Pot";
+    // Round to 2 decimal places
+    payout.value = (pot / 2).toFixed(2) + "€";
+}
+
+
 for (let user of users_element.children) {
     user.children[0].addEventListener('click', function(event) {
         const element = event.currentTarget.parentElement;
@@ -12,6 +39,7 @@ for (let user of users_element.children) {
             selected_users.removeChild(element);
             users_element.appendChild(element);
         }
+        updatePot();
 
         if (users_element.children.length === 0) {
             selected_users.style.borderBottom = 'none';
