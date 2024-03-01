b46df3354f0a9cfe0a058169c695d838397ba2f0
staubtornado <76395253+staubtornado@users.noreply.github.com>
2024-03-01 16:41:47 +0100

    Added function for requests to server

---
+++ src/main/resources/static/js/components/networking.js
@@ -0,0 +1,24 @@
+/**
+ * This function sends a request to the server.
+ * @param {string} data - The data to send in the request. Should be in the form of {key: value, key: value, ...}
+ * @param {string} url - The URL to send the request to.
+ * @param {string} method - The method to use for the request.
+ * @param {string} contentType - The content type of the request.
+ * @param {function} callback - The function to call when the request is complete. Should have three parameters:
+ *                              response, status, and isOK.
+ * @returns {void}
+ */
+export function sendDataToServer(data, url, method, contentType, callback) {
+    fetch(url, {
+        method: method,
+        headers: {
+            'Content-Type': contentType
+        },
+        body: data
+    })
+    .then(response => {
+        response.text().then(text => {
+            callback(text, response.status, response.ok);
+        });
+    });
+}
